a076455230aa7f66e20e6eccd916403524e54d37
Tomaszius <liutkus.to@gmail.com>
2022-06-27 19:43:45 +0300

    pabaigta antra uzduotis

--- src/uzduotis2.java
+++ src/uzduotis2.java
@@ -1,2 +1,29 @@
 public class uzduotis2 {
+    public static void main(String[] args) {
+
+        int x = 2;
+        int y = 110;
+
+        System.out.println(funkcija(x, y));
+    }
+
+    public static int funkcija (int x, int y){
+        if(x % 2 == 0){
+            return x * y;
+        }
+        else if(x % 2 == 1){
+            if(y > 100){
+                return y - x;
+            }
+            else if(y < 100){
+                return x - y;
+            }
+            else if(y == 100){
+                return 0;
+            }
+        }
+        else{
+        }
+        return 1993;
+    }
 }
